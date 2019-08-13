882381f6864e4f527a0c26dca074dff2db1ba2a3
KanzariAmine <kanzari.amin@gmail.com>
2019-08-13 21:50:18 +0100

    Install the dependencies && add fireBase config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+  apiKey: "AIzaSyCfrroVw04-1nSXLd1q4iAHu4s4mFMgziE",
+  authDomain: "evernote-clone-cc2a1.firebaseapp.com",
+  databaseURL: "https://evernote-clone-cc2a1.firebaseio.com",
+  projectId: "evernote-clone-cc2a1",
+  storageBucket: "evernote-clone-cc2a1.appspot.com",
+  messagingSenderId: "1098892673880",
+  appId: "1:1098892673880:web:562ed72bec36f71a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
